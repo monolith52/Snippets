48d67ca427e7f85b0b9ac541b0d099ca81ad2d58
Monolith52 <monolith52@gmail.com>
2017-04-22 02:02:44 +0900

    説明用コメントを追加

--- src/main/java/textformatter/Controller.java
+++ src/main/java/textformatter/Controller.java
@@ -21,8 +21,10 @@ public class Controller {
     private void initialize() {
         TextFormatter<Number> currencyFormatter = new TextFormatter<>(
                 new CurrencyStringConverter(Locale.JAPAN), 0, change -> {
+            // 選択範囲は1文字目より前にできない
             change.setAnchor(Math.max(1, change.getAnchor()));
             change.setCaretPosition(Math.max(1, change.getCaretPosition()));
+            // テキスト変更範囲は1文字目より前にできない
             change.setRange(Math.max(1, change.getRangeStart()), Math.max(1, change.getRangeEnd()));
             return change;
         });
