9539d8d0d490b70a600d24f4f1fedfef3b12eeb4
Monolith52 <monolith52@gmail.com>
2017-04-22 01:02:13 +0900

    textformatter サンプルを追加

---
+++ src/main/java/textformatter/Controller.java
@@ -0,0 +1,37 @@
+package textformatter;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.util.converter.CurrencyStringConverter;
+import javafx.util.converter.NumberStringConverter;
+
+import java.util.Locale;
+
+public class Controller {
+
+    @FXML
+    private TextField textField1;
+    @FXML
+    private TextField textField2;
+
+    @FXML
+    private void initialize() {
+        TextFormatter<Number> currencyFormatter = new TextFormatter<>(
+                new CurrencyStringConverter(Locale.JAPAN), 0, change -> {
+            change.setAnchor(Math.max(1, change.getAnchor()));
+            change.setCaretPosition(Math.max(1, change.getCaretPosition()));
+            change.setRange(Math.max(1, change.getRangeStart()), Math.max(1, change.getRangeEnd()));
+            return change;
+        });
+        textField1.setTextFormatter(currencyFormatter);
+
+        TextFormatter<String> lowerFormatter = new TextFormatter<>(change -> {
+            change.setText(change.getText().toLowerCase());
+            return change;
+        });
+        textField2.setTextFormatter(lowerFormatter);
+    }
+}
--- src/main/java/textformatter/Main.java
+++ src/main/java/textformatter/Main.java
@@ -1,4 +1,4 @@
-package customtable;
+package textformatter;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
